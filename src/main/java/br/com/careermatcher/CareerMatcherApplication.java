95d34841ce0823eeb79dfae02746f131d5844f57
Natan Ferreira <natan16ferreira@gmail.com>
2025-11-04 12:54:22 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/careermatcher/CareerMatcherApplication.java
@@ -0,0 +1,13 @@
+package br.com.careermatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerMatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerMatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/careermatcher/CareerMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.careermatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerMatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
